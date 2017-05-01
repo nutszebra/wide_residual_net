e677920036d1549701468643dbbcffce2530905d
nutszebra <nutszebra@gmail.com>
2017-05-01 23:16:14 +0900

    sgd momentum

--- nutszebra_optimizer.py
+++ nutszebra_optimizer.py
@@ -60,9 +60,10 @@ class OptimizerWideRes(Optimizer):
 
 class OptimizerGooglenetV3(Optimizer):
 
-    def __init__(self, model=None, lr=0.045, decay=0.9, weight_decay=1.0e-6, clip=2.0):
+    def __init__(self, model=None, lr=0.045, weight_decay=1.0e-6, clip=2.0):
         super(OptimizerGooglenetV3, self).__init__(model)
-        optimizer = optimizers.RMSprop(lr, decay)
+        # optimizer = optimizers.RMSprop(lr, decay)
+        optimizer = optimizers.MomentumSGD(lr, 0.9)
         weight_decay = chainer.optimizer.WeightDecay(weight_decay)
         clip = chainer.optimizer.GradientClipping(clip)
         optimizer.setup(self.model)
